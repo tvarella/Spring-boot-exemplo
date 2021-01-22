8e83762a105bfa2ca51d699a53d66eb9364201b3
Varella <thiago_varella@yahoo.com>
2021-01-22 00:17:32 -0200

    retirando sysout

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -15,8 +15,6 @@ public class ForumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
-		
-		System.out.println("teste do tio");
 	}
 
 }
